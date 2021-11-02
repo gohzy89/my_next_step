71af25c35460d0036e7e54b956e582183d6ea4f9
gohzy89 <gohzy89@gmail.com>
2021-11-02 16:11:43 +0800

    changes to browse

--- lib/browse.dart
+++ lib/browse.dart
@@ -11,71 +11,188 @@ class browse extends StatefulWidget {
 }
 
 class _browseState extends State<browse> {
-  
-  String returnschool(String value,int index){
-    switch(value){
-      case "School":return accinfo.allcourse[accinfo.allcourse.keys.elementAt(index)]["school_name"];
-      case 'Cut-Off':return accinfo.allcourse[accinfo.cutofflist[index]]["school_name"];
-      case 'Alphabetical':return accinfo.allcourse[accinfo.alphabetlist[index]]["school_name"];
+  String returnschool(String value, int index) {
 
-    }
-    return "";
-  }
-  String returncutoff(String value,int index){
-    switch(value){
-      case "School":return accinfo.allcourse[accinfo.allcourse.keys.elementAt(index)]["courseCutOff"];
-      case 'Cut-Off':return accinfo.allcourse[accinfo.cutofflist[index]]["courseCutOff"];
-      case 'Alphabetical':return accinfo.allcourse[accinfo.alphabetlist[index]]["courseCutOff"];
-
+    switch (value) {
+      case "School":
+        return accinfo.allcourse[accinfo.allcourse.keys.elementAt(index)]
+            ["school_name"];
+      case 'Cut-Off':
+        return accinfo.allcourse[accinfo.cutofflist[index]]["school_name"];
+      case 'Alphabetical':
+        return accinfo.allcourse[accinfo.alphabetlist[index]]["school_name"];
     }
     return "";
   }
 
-  String returncourse(String value,int index){
-    switch(value){
-      case "School":return accinfo.allcourse[accinfo.allcourse.keys.elementAt(index)]["courseName"];
-      case 'Cut-Off':return accinfo.allcourse[accinfo.cutofflist[index]]["courseName"];
-      case 'Alphabetical':return accinfo.allcourse[accinfo.alphabetlist[index]]["courseName"];
-
-    }
-    return "";
-  }
-  String returncode(String value,int index){
-    switch(value){
-      case "School":return accinfo.allcourse.keys.elementAt(index);
-      case 'Cut-Off':return accinfo.cutofflist[index];
-      case 'Alphabetical':return accinfo.alphabetlist[index];
-
+  String returncutoff(String value, int index) {
+    switch (value) {
+      case "School":
+        return accinfo.allcourse[accinfo.allcourse.keys.elementAt(index)]
+            ["courseCutOff"];
+      case 'Cut-Off':
+        return accinfo.allcourse[accinfo.cutofflist[index]]["courseCutOff"];
+      case 'Alphabetical':
+        return accinfo.allcourse[accinfo.alphabetlist[index]]["courseCutOff"];
     }
     return "";
   }
 
-  List<String> searchresult=[];
-  
-  String filtervalue ='School';
+  String returncourse(String value, int index) {
+    switch (value) {
+      case "School":
+        return accinfo.allcourse[accinfo.allcourse.keys.elementAt(index)]
+            ["courseName"];
+      case 'Cut-Off':
+        return accinfo.allcourse[accinfo.cutofflist[index]]["courseName"];
+      case 'Alphabetical':
+        return accinfo.allcourse[accinfo.alphabetlist[index]]["courseName"];
+    }
+    return "";
+  }
+
+  String returncode(String value, int index) {
+    switch (value) {
+      case "School":
+        return accinfo.allcourse.keys.elementAt(index);
+      case 'Cut-Off':
+        return accinfo.cutofflist[index];
+      case 'Alphabetical':
+        return accinfo.alphabetlist[index];
+    }
+    return "";
+  }
+  void alphabetsort(List<String> list){
+    Map alphabet = new Map();
+    for (int i = 0; i < list.length; i++) {
+      alphabet[list[i]] = searchresult[i];
+    }
+    list.sort();
+    for (int i = 0; i < list.length; i++) {
+      list.insert(i, alphabet[list[i]]);
+      list.removeAt(i + 1);
+    }
+  }
+  void inserttocutoff(String code) {
+    if (searchresultcutoff.length == 0) {
+      searchresultcutoff.add(code);
+      return;
+    }
+    for (int i = 0; i < searchresultcutoff.length; i++) {
+      if (int.parse(accinfo.allcourse[code]["courseCutOff"]) <=
+          int.parse(accinfo.allcourse[searchresultcutoff[i]]["courseCutOff"])) {
+        searchresultcutoff.insert(i, code);
+        return;
+      }
+    }
+    searchresultcutoff.insert(searchresultcutoff.length, code);
+  }
+
+  List<String> searchresult = [];
+  List<String> searchresultcutoff = [];
+  List<String> searchresultalphabet = [];
+  late List<Padding> returnedlist ;
+  String filtervalue = 'School';
   Future<List<Widget>> search(String? search) async {
     await Future.delayed(Duration(seconds: 2));
-    searchresult = [];
-    for(int i=0;i<accinfo.allcourse.length;i++){
-      if(accinfo.allcourse[accinfo.allcourse.keys.elementAt(i)]["courseName"].toLowerCase().contains(search?.toLowerCase()))
-        searchresult.add(accinfo.allcourse.keys.elementAt(i));
+    if(searchresult.length==0)
+    {
+      searchresult = [];
+      searchresultcutoff = [];
+      searchresultalphabet = [];
+      for (int i = 0; i < accinfo.allcourse.length; i++) {
+        if (accinfo.allcourse[accinfo.allcourse.keys.elementAt(i)]["courseName"]
+            .toLowerCase()
+            .contains(search?.toLowerCase())) {
+          searchresult.add(accinfo.allcourse.keys.elementAt(i));
+          inserttocutoff(accinfo.allcourse.keys.elementAt(i));
+          searchresultalphabet.add(
+              accinfo.allcourse[accinfo.allcourse.keys.elementAt(i)]
+                      ["courseName"] +
+                  accinfo.allcourse.keys.elementAt(i));
+        }
+      }
+      alphabetsort(searchresultalphabet);
     }
-    return List.generate(searchresult.length, (int index) {
+    return searchresultfilter(filtervalue);
+  }
+
+  List<Padding> searchresultfilter(String value){
+    if(value=="School")
+    return  List.generate(searchresult.length, (int index) {
       return Padding(
         padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
         child: ListTile(
-          onTap: (){
-            Navigator.push(context, MaterialPageRoute(builder: (context) => selected(searchresult[index])));
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => selected(searchresult[index])));
           },
-          title: Text("School: ${accinfo.allcourse[searchresult[index]]["school_name"]} \nCourse: ${accinfo.allcourse[searchresult[index]]["courseName"]}\nCut-off:${accinfo.allcourse[searchresult[index]]["courseCutOff"]}"),
+          title: Text(
+              "School: ${accinfo.allcourse[searchresult[index]]["school_name"]} \nCourse: ${accinfo.allcourse[searchresult[index]]["courseName"]}\nCut-off:${accinfo.allcourse[searchresult[index]]["courseCutOff"]}"),
           leading: Image.asset('assets/images/${searchresult[index][0]}.jpg'),
           shape: RoundedRectangleBorder(
 
             // side: BorderSide(color: Colors.red,width: 3)  have to figure out how to implement border color
-          )
-          ,
+          ),
           tileColor: Colors.white,
-          trailing: Icon(Icons.arrow_forward_outlined,color: Colors.black,),),
+          trailing: Icon(
+            Icons.arrow_forward_outlined,
+            color: Colors.black,
+          ),
+        ),
+      );
+    });
+    else if (value=='Alphabetical')
+    return List.generate(searchresultalphabet.length, (int index) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+        child: ListTile(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => selected(searchresultalphabet[index])));
+          },
+          title: Text(
+              "School: ${accinfo.allcourse[searchresultalphabet[index]]["school_name"]} \nCourse: ${accinfo.allcourse[searchresultalphabet[index]]["courseName"]}\nCut-off:${accinfo.allcourse[searchresultalphabet[index]]["courseCutOff"]}"),
+          leading: Image.asset('assets/images/${searchresultalphabet[index][0]}.jpg'),
+          shape: RoundedRectangleBorder(
+
+            // side: BorderSide(color: Colors.red,width: 3)  have to figure out how to implement border color
+          ),
+          tileColor: Colors.white,
+          trailing: Icon(
+            Icons.arrow_forward_outlined,
+            color: Colors.black,
+          ),
+        ),
+      );
+    });
+    return List.generate(searchresultcutoff.length, (int index) {
+      return Padding(
+        padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+        child: ListTile(
+          onTap: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => selected(searchresultcutoff[index])));
+          },
+          title: Text(
+              "School: ${accinfo.allcourse[searchresultcutoff[index]]["school_name"]} \nCourse: ${accinfo.allcourse[searchresultcutoff[index]]["courseName"]}\nCut-off:${accinfo.allcourse[searchresultcutoff[index]]["courseCutOff"]}"),
+          leading: Image.asset('assets/images/${searchresultcutoff[index][0]}.jpg'),
+          shape: RoundedRectangleBorder(
+
+            // side: BorderSide(color: Colors.red,width: 3)  have to figure out how to implement border color
+          ),
+          tileColor: Colors.white,
+          trailing: Icon(
+            Icons.arrow_forward_outlined,
+            color: Colors.black,
+          ),
+        ),
       );
     });
   }
@@ -88,9 +205,7 @@ class _browseState extends State<browse> {
       width: MediaQuery.of(context).size.width,
       child: Material(
         child: SafeArea(
-          child: Stack(
-              children: [
-
+          child: Stack(children: [
             Scaffold(
               resizeToAvoidBottomInset: false,
               appBar: AppBar(
@@ -102,52 +217,69 @@ class _browseState extends State<browse> {
                   mainAxisAlignment: MainAxisAlignment.start,
                   children: [
                     IconButton(
-                      icon: Icon(Icons.arrow_back,color: Colors.black,),
+                      icon: Icon(
+                        Icons.arrow_back,
+                        color: Colors.black,
+                      ),
                       onPressed: () {
                         Navigator.pop(context);
                       },
                       iconSize: 40,
                     ),
-                    Text("Back",style: TextStyle(color: Colors.black),)
+                    Text(
+                      "Back",
+                      style: TextStyle(color: Colors.black),
+                    )
                   ],
                 ),
-
               ),
               body: SearchBar<Widget>(
-hintText: "Search by Course Name",                placeHolder: ListView.separated(
-                    itemCount: accinfo.allcourse.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Padding(
-                        padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
-                        child: ListTile(
-                          onTap: (){
-                            Navigator.push(context, MaterialPageRoute(builder: (context) => selected(returncode(filtervalue, index))));
-                          },
-                            title: Text("School: ${returnschool(filtervalue, index)} \nCourse: ${returncourse(filtervalue, index)}\nCut-off:${returncutoff(filtervalue, index)}"),
-                          leading: Image.asset('assets/images/${returncode(filtervalue, index)[0]}.jpg'),
-                          shape: RoundedRectangleBorder(
+                hintText: "Search by Course Name",
+                placeHolder: ListView.separated(
+                  itemCount: accinfo.allcourse.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 3, 0, 3),
+                      child: ListTile(
+                        onTap: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => selected(
+                                      returncode(filtervalue, index))));
+                        },
+                        title: Text(
+                            "School: ${returnschool(filtervalue, index)} \nCourse: ${returncourse(filtervalue, index)}\nCut-off:${returncutoff(filtervalue, index)}"),
+                        leading: Image.asset(
+                            'assets/images/${returncode(filtervalue, index)[0]}.jpg'),
+                        shape: RoundedRectangleBorder(
 
-                             // side: BorderSide(color: Colors.red,width: 3)  have to figure out how to implement border color
-                          )
-                          ,
-                            tileColor: Colors.white,
-                            trailing: Icon(Icons.arrow_forward_outlined,color: Colors.black,),),
-                      );
-                    },
-                    separatorBuilder: (context,index) {
-              return Padding(
-              padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
-              child: Container(
-              padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
-              height: 1,
-              color: Colors.grey,
-              ),
-              );
-              },),
+                            // side: BorderSide(color: Colors.red,width: 3)  have to figure out how to implement border color
+                            ),
+                        tileColor: Colors.white,
+                        trailing: Icon(
+                          Icons.arrow_forward_outlined,
+                          color: Colors.black,
+                        ),
+                      ),
+                    );
+                  },
+                  separatorBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.fromLTRB(0, 5, 0, 5),
+                      child: Container(
+                        padding: EdgeInsets.fromLTRB(0, 5, 0, 5),
+                        height: 1,
+                        color: Colors.grey,
+                      ),
+                    );
+                  },
+                ),
                 onSearch: search,
                 onItemFound: (Widget? tile, int index) {
                   return tile!;
                 },
+                onCancelled: (){searchresult=[];},
               ),
             ),
             Container(
@@ -174,7 +306,6 @@ hintText: "Search by Course Name",                placeHolder: ListView.separate
                 }).toList(),
               ),
             ),
-
           ]),
         ),
       ),
